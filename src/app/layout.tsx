db4a524fb7fcf564afdd0153250fb0e97cbed241
NataliaGurova <nyxnata@ukr.net>
2025-05-26 19:26:26 +0300

    reStart2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
 
 
 
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+// import Footer from "@/components/Footer";
+// import Header from "@/components/Header";
 
 
 export const metadata: Metadata = {
---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+
+const Logo = () => {
+  return (
+    <Link href="/">
+      <h2>LOGO</h2>
+    </Link>
+  )
+}
+
+export default Logo
