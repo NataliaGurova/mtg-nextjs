da83f29d8dfb0be85f0ac9d5b582d488473c2166
NataliaGurova <nyxnata@ukr.net>
2025-06-03 14:45:36 +0300

    SideMenu & NavbarMobil

--- src/components/navbar-mobil/NavbarMobil.tsx
+++ src/components/navbar-mobil/NavbarMobil.tsx
@@ -1,29 +1,41 @@
 "use client";
 
 import React, { useState } from 'react'
-import { AlignLeft } from 'lucide-react';
+import { AlignRight } from 'lucide-react';
 import SideMenu from '../side-menu/SideMenu';
+import { X } from 'lucide-react';
 
 const NavbarMobil = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <>
-      <button onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-        {/* <AlignJustify/> */}
-        <AlignLeft className="text-main-text md:hidden hover:cursor-pointer" />
-      </button>
-      {/* <button onClick={onClose} className="p-2">
-        X
-      </button> */}
       <div className="md:hidden">
         <SideMenu
           isOpen={isSidebarOpen}
           onClose={() => setIsSidebarOpen(false)}
         />
       </div>
+      <button
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        title="Menu"
+        className="fixed top-4 right-5 z-50 md:hidden"
+      >
+        {isSidebarOpen ? (
+          <X className="text-main-text w-6 h-6" />
+        ) : (
+          <AlignRight className="text-main-text w-6 h-6" />
+        )}
+      </button>
+      {/* <button onClick={() => setIsSidebarOpen(!isSidebarOpen)} title="Menu">
+        <AlignRight className="text-main-text md:hidden hover:cursor-pointer" />
+      </button> */}
+        {/* <AlignJustify/> */}
+      {/* <button onClick={onClose} className="p-1">
+        <X />
+      </button> */}
     </>
   )
 }
 
-export default NavbarMobil
+export default NavbarMobil;
