208184fadec6353f1cc3ec5715c0e20f742bd73e
NataliaGurova <nyxnata@ukr.net>
2025-06-03 11:58:23 +0300

    Header up

--- src/components/sign-up/SignUp.tsx
+++ src/components/sign-up/SignUp.tsx
@@ -1,13 +1,26 @@
+'use client'
+
+import clsx from 'clsx';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
 const SignUp = () => {
+  const pathname = usePathname();
+  const isActive = pathname === "/register";
+  
   return (
     <Link
-      href="/register">
-        <span className="hidden md:inline">/Register</span>
+      href="/register"
+      className={clsx(
+        "relative group border-b-[2px] pb-1 transition-colors",
+        isActive ? "text-nav-yellow border-nav-yellow" : "border-transparent hover:border-main-text"
+      )}
+      title="Register"
+    >
+        <span className="inline">Register</span>
     </Link>
   )
 }
 
-export default SignUp
+export default SignUp;
