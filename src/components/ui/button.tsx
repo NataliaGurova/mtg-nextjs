94ec8ea061cc8b1021eb69b9b7ca23adef615538
NataliaGurova <nyxnata@ukr.net>
2026-02-10 20:00:14 +0200

    end

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,6 +5,22 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 
+
+type ButtonProps =
+  (
+    | (React.ComponentPropsWithoutRef<"button"> & {
+        asChild?: false
+      })
+    | (React.ComponentPropsWithoutRef<typeof Slot> & {
+        asChild: true
+      })
+  ) &
+  VariantProps<typeof buttonVariants>
+
+
+
+
+
 // const buttonVariants = cva(
 //   "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 outline-none focus-visible:ring-2 focus-visible:ring-ring",
 //   {
@@ -104,29 +120,55 @@ const buttonVariants = cva(
 );
 
 
-
 function Button({
   className,
   variant,
   size,
   asChild = false,
   ...props
-}: React.ComponentProps<"button"> &
-VariantProps<typeof buttonVariants> & {
-  asChild?: boolean
-}) {
+}: ButtonProps) {
   const Comp = asChild ? Slot : "button"
-  
+
   return (
     <Comp
-    data-slot="button"
-    className={cn(buttonVariants({ variant, size, className }))}
-    {...props}
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
     />
   )
 }
 
 export { Button, buttonVariants }
+
+
+
+// function Button({
+//   className,
+//   variant,
+//   size,
+//   asChild = false,
+//   ...props
+// }: React.ComponentProps<"button"> &
+// VariantProps<typeof buttonVariants> & {
+//   asChild?: boolean
+// }) {
+//   const Comp = asChild ? Slot : "button"
+  
+//   return (
+//     <Comp
+//     data-slot="button"
+//     className={cn(buttonVariants({ variant, size, className }))}
+//     {...props}
+//     />
+//   )
+// }
+
+// export { Button, buttonVariants }
+
+
+
+
+// ======================================================
 
 // const buttonVariants = cva(
 //   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
