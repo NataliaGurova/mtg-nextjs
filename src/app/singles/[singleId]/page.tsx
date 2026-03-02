db0cef91b9fa3c8386edbe8454d04029b9e2e9a1
NataliaGurova <nyxnata@ukr.net>
2026-03-02 15:10:07 +0200

    Update page.tsx

--- src/app/singles/[singleId]/page.tsx
+++ src/app/singles/[singleId]/page.tsx
@@ -4,12 +4,16 @@ import CardDetails from "@/components/cards/CardDetails/CardDetails";
 import Container from "@/components/Container/Container";
 import { getCardsByScryfallId } from "@/db/repositories/card.repository";
 
+// interface PageProps {
+//   params: Promise<{ singleId: string }>;
+// }
 interface PageProps {
-  params: Promise<{ singleId: string }>;
+  params: { singleId: string };
 }
 
 export default async function SingleDetailsPage({ params }: PageProps) {
-  const { singleId } = await params;
+  // const { singleId } = await params;
+  const { singleId } = params;
 
   const cards = await getCardsByScryfallId(singleId);
 
