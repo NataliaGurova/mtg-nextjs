3e6880c22a103b8c7cf34dc921b7ae57667a20de
NataliaGurova <nyxnata@ukr.net>
2026-03-02 16:42:23 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/profile", "/orders"],
+  matcher: ["/profile", "/account/:path*"],
 };
 
 // все пути, которые нужно защитить с помощью аутентификации, можно добавить в массив matcher выше
