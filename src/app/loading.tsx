ebc35bfb3bb7da3b59566d56a7380647e2528538
NataliaGurova <nyxnata@ukr.net>
2026-05-03 12:52:35 +0300

    input

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,12 @@
+// src/app/loading.tsx
+
 import Loader from '@/components/Loader/Loader';
 import React from 'react'
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center py-30">
+    // <div className="flex items-center justify-center py-30">
+    <div className="flex items-center justify-center min-h-[60vh]">
       <Loader />
     </div>
   )
